19b5e189cf1272933810cf357a3b7f5090b000e6
pinqy <19714467+pinqy@users.noreply.github.com>
2024-01-15 20:01:40 -0500

    Error fix

--- games_common/src/main/java/com/pinqy/games/games_common/Main.java
+++ games_common/src/main/java/com/pinqy/games/games_common/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! This just shows a test frame...");
 
-        GameFrame game = new GameFrame(new GamePage(), false);
+        GameFrame game = new GameFrame(new GamePage("test"), false);
         game.show();
     }
 }
